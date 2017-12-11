da92a748924e0f64f8e75d2d9eba2971c05b969f
Napon Zupat <napon_zupat@hotmail.com>
2017-12-11 23:25:27 +0700

    edit intent

--- app/src/main/java/com/example/navadroid/androidedittextvalidation/Option1Activity.java
+++ app/src/main/java/com/example/navadroid/androidedittextvalidation/Option1Activity.java
@@ -55,6 +55,7 @@ public class Option1Activity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 validateEditText(); // OR validation can be specific (only for this EditText)
+                validateName();
             }
         });
 
@@ -72,11 +73,46 @@ public class Option1Activity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 // TODO: add your Password validation here
+                validateEditText();
+                validatePwd();
             }
         });
 
-//        etPhone.addTextChangedListener(...);
-//        etEmail.addTextChangedListener(...);
+        etEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                validateEditText();
+                validateEmail();
+            }
+        });
+
+        etPhone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                validateEditText();
+                validatePhone();
+            }
+        });
     }
 
     // To validate all EditTexts
@@ -87,7 +123,63 @@ public class Option1Activity extends AppCompatActivity {
             isValidated = false;
         }
         // TODO: add your EditText validation here
+        if (etPwd.getText().toString().length() == 0) {
+            etPwd.setError("Required");
+            isValidated = false;
+        }
+        if (etEmail.getText().toString().length() == 0) {
+            etEmail.setError("Required");
+            isValidated = false;
+        }
+        if (etPhone.getText().toString().length() == 0) {
+            etPhone.setError("Required");
+            isValidated = false;
+        }
 
         return isValidated;
+    }
+
+    private boolean validateName() {
+        boolean nameIsValidated = true;
+        String text = etName.getText().toString();
+        if (!text.matches("^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$")) {
+            nameIsValidated = false;
+            etName.setError("Invalid format");
+        }
+
+        return nameIsValidated;
+    }
+
+    private boolean validatePwd() {
+        boolean pwdIsValidated = true;
+        String text = etPwd.getText().toString();
+        if (!text.matches("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,13})")){
+            pwdIsValidated = false;
+            etPwd.setError("Require 6-13 char,[a-z],[A-Z],[0-9]");
+        }
+
+        return pwdIsValidated;
+    }
+
+    private boolean validateEmail() {
+        boolean emailIsValidated = true;
+        String text = etEmail.getText().toString();
+        if (!text.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x07\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x07\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
+            emailIsValidated = false;
+            etEmail.setError("Invalid, hint: example@email.com");
+        }
+
+        return emailIsValidated;
+    }
+
+    private boolean validatePhone() {
+
+        boolean phoneIsValidated = true;
+        String text = etPhone.getText().toString();
+        if (!text.matches("^(?:0091|\\\\+91|0)[7-9][0-9]{7,8}$")) {
+            phoneIsValidated = false;
+            etPhone.setError("Invalid format");
+        }
+        return phoneIsValidated;
     }
 }
--- app/src/main/java/com/example/navadroid/androidedittextvalidation/Option3Activity.java
+++ app/src/main/java/com/example/navadroid/androidedittextvalidation/Option3Activity.java
@@ -34,7 +34,7 @@ public class Option3Activity extends AppCompatActivity {
         findViewById(R.id.btn_validate3).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(validateEditText()){
+                if(validateEditText() && validateName() && validatePwd() && validateEmail() && validatePhone()){
                     Toast.makeText(Option3Activity.this, "Okay. You are good to go.", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -44,9 +44,8 @@ public class Option3Activity extends AppCompatActivity {
         etName.addTextChangedListener(new TextValidator(etName) {
             @Override
             public void validate(TextView textView, String text) {
-                if (etName.getText().toString().length() == 0) {
-                    etName.setError("Required");
-                }
+                validateEditText();
+                validateName();
             }
         });
 
@@ -54,11 +53,26 @@ public class Option3Activity extends AppCompatActivity {
             @Override
             public void validate(TextView textView, String text) {
                 // TODO: add your Password validation here
+                validateEditText();
+                validatePwd();
             }
         });
 
-        //etEmail.addTextChangedListener(...);
-        //etPhone.addTextChangedListener(...);
+        etEmail.addTextChangedListener(new TextValidator(etEmail) {
+            @Override
+            public void validate(TextView textView, String text) {
+                validateEditText();
+                validateEmail();
+            }
+        });
+
+        etPhone.addTextChangedListener(new TextValidator(etPhone) {
+            @Override
+            public void validate(TextView textView, String text) {
+                validateEditText();
+                validatePhone();
+            }
+        });
     }
 
 
@@ -70,7 +84,63 @@ public class Option3Activity extends AppCompatActivity {
             isValidated = false;
         }
         // TODO: add your EditText validation here
+        if (etPwd.getText().toString().length() == 0) {
+            etPwd.setError("Required");
+            isValidated = false;
+        }
+        if (etEmail.getText().toString().length() == 0) {
+            etEmail.setError("Required");
+            isValidated = false;
+        }
+        if (etPhone.getText().toString().length() == 0) {
+            etPhone.setError("Required");
+            isValidated = false;
+        }
 
         return isValidated;
+    }
+
+    private boolean validateName() {
+        boolean nameIsValidated = true;
+        String text = etName.getText().toString();
+        if (!text.matches("^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$")) {
+            nameIsValidated = false;
+            etName.setError("Invalid format");
+        }
+
+        return nameIsValidated;
+    }
+
+    private boolean validatePwd() {
+        boolean pwdIsValidated = true;
+        String text = etPwd.getText().toString();
+        if (!text.matches("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,13})")){
+            pwdIsValidated = false;
+            etPwd.setError("Require 6-13 char,[a-z],[A-Z],[0-9]");
+        }
+
+        return pwdIsValidated;
+    }
+
+    private boolean validateEmail() {
+        boolean emailIsValidated = true;
+        String text = etEmail.getText().toString();
+        if (!text.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x07\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x07\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
+            emailIsValidated = false;
+            etEmail.setError("Invalid, hint: example@email.com");
+        }
+
+        return emailIsValidated;
+    }
+
+    private boolean validatePhone() {
+
+        boolean phoneIsValidated = true;
+        String text = etPhone.getText().toString();
+        if (!text.matches("^(?:0091|\\\\+91|0)[7-9][0-9]{7,8}$")) {
+            phoneIsValidated = false;
+            etPhone.setError("Invalid format");
+        }
+        return phoneIsValidated;
     }
 }
